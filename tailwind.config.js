f7a1428068af41858ce96577bbdec833934fa5a0
Nikhil Vishwakarma <63867254+vishwakarmanikhil@users.noreply.github.com>
2026-03-13 23:30:18 +0530

    config fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
+module.exports = {
   darkMode: "class",
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {},
-    },
+    extend: {},
   },
   plugins: [],
 };
-
-export default config;
